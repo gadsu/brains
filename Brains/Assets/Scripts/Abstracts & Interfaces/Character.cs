14b7b82f5a81090569d92e00037379e71c77f317
Paul Manley <pjmanley671@gmail.com>
2018-08-28 21:18:53 -0500

    Changing abstract to interface and adding namespace.

---
+++ Brains/Assets/Scripts/Abstracts & Interfaces/Character.cs
@@ -0,0 +1,22 @@
+﻿using Assets.Scripts.Abstracts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+interface ICharacter
+{
+    MovementState MVState { get; set; }
+
+    void Move(Rigidbody rbody, Vector3 moveDir, MovementState mState);
+    void AnimateMovement(Animation anim);
+}
+
+namespace Assets.Scripts.Abstracts
+{
+    public enum MovementState
+    {
+        Idle,
+        Creep,
+        Crawl
+    }
+}
---
+++ Brains/Assets/Scripts/Player/Player.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Assets.Scripts.Abstracts;
+using UnityEngine;
+
+public class Player : MonoBehaviour, ICharacter {
+    private MovementState mvState;
+    public MovementState MVState
+    {
+        get
+        {
+            return mvState;
+        }
+
+        set
+        {
+            mvState = value;
+        }
+    }
+
+    public void AnimateMovement(Animation anim)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void Move(Rigidbody rbody, Vector3 moveDir, MovementState mState)
+    {
+        throw new System.NotImplementedException();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
