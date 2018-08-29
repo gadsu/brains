fbd0a870288ff497379625ff65ca4c7bb7fb536a
Paul Manley <pjmanley671@gmail.com>
2018-08-29 16:03:48 -0500

    Another try at abstraction.

---
+++ Brains/Assets/Scripts/Abstracts & Interfaces/ACharacter.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class ACharacter : MonoBehaviour
+{
+    public enum MovementState
+    {
+        Idling,
+        Crawling,
+        Creeping
+    }
+
+    private MovementState mvState;
+    private Animation _anim;
+    private bool animStateChange;
+
+    protected MovementState MvState
+    {
+        get
+        {
+            return mvState;
+        }
+
+        set
+        {
+            mvState = value;
+        }
+    }
+    protected Animation Anim
+    {
+        get
+        {
+            return _anim;
+        }
+
+        set
+        {
+            _anim = value;
+        }
+    }
+    protected bool AnimStateChange
+    {
+        get
+        {
+            return animStateChange;
+        }
+
+        set
+        {
+            animStateChange = value;
+        }
+    }
+
+    protected abstract Vector3 Move();
+    protected abstract void Animate();
+}
