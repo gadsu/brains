b22b4efb6bdbca0fd56b7f0269324c992c631357
Paul Manley <pjmanley671@gmail.com>
2018-09-04 19:19:39 -0500

    Small adjustments plus build v 0.1a

--- Brains/Assets/Scripts/Abstracts & Interfaces/ACharacter.cs
+++ Brains/Assets/Scripts/Abstracts & Interfaces/ACharacter.cs
@@ -7,8 +7,8 @@ public abstract class ACharacter : MonoBehaviour
     public enum MovementState
     {
         Idling = 0,
-        Creeping = 5,
-        Crawling = 10
+        Crawling = 5,
+        Creeping = 10
     }
 
     private MovementState mvState;
