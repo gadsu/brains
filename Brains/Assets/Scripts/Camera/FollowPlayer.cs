e65aabe9d02bd371c322a53cfb1733fcfcd95df9
Paul Manley <pjmanley671@gmail.com>
2018-09-10 17:03:52 -0500

    Changes changing changingly

---
+++ Brains/Assets/Scripts/Camera/FollowPlayer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+    public float offsetX, offsetY, offsetZ;
+    public GameObject target;
+
+    private Vector3 pos;
+    private Quaternion rot;
+
+    private void Start()
+    {
+        pos = transform.position;
+        rot = gameObject.transform.rotation;
+    }
+
+    private void Update()
+    {
+        rot = transform.rotation;
+
+        pos = target.transform.position;
+        pos.x = pos.x - offsetX;
+        pos.y = pos.y - offsetY;
+        pos.z = pos.z - offsetZ;
+
+
+
+        transform.position = pos;
+    }
+}
