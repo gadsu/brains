88b757f335bd3d06802dc01db15bc2e829aedeaa
Rungostur <detrian.hallem@trojans.dsu.edu>
2018-09-12 16:19:38 -0500

    Revert "Merge branch 'Develop' of https://github.com/gadsu/brains into Develop"
    This reverts commit 9e405bc6ea5832f5f2c018ce9fb67ff3749b45a0, reversing
    changes made to 03fcd87107b073020cc364af220d82ed26a91710.

--- Brains/Assets/Scripts/Camera/FollowPlayer.cs
+++ Brains/Assets/Scripts/Camera/FollowPlayer.cs
@@ -4,41 +4,29 @@ using UnityEngine;
 
 public class FollowPlayer : MonoBehaviour
 {
+    public float offsetX, offsetY, offsetZ;
     public GameObject target;
-    private Vector3 targetPos;
+
     private Vector3 pos;
     private Quaternion rot;
 
-    private Vector3 dist;
-    /*private float distX;
-    private float distY;
-    private float distZ;*/
     private void Start()
     {
-        targetPos = target.GetComponent<Transform>().position;
         pos = transform.position;
-        dist = new Vector3(0, 0, 0);
+        rot = gameObject.transform.rotation;
     }
 
     private void Update()
     {
-        pos = transform.position;
+        rot = transform.rotation;
 
-        dist.x = targetPos.x - pos.x;
-        dist.y = targetPos.y - pos.y;
-        dist.z = targetPos.z - pos.z;
+        pos = target.transform.position;
+        pos.x = pos.x - offsetX;
+        pos.y = pos.y - offsetY;
+        pos.z = pos.z - offsetZ;
 
-        targetPos = target.GetComponent<Transform>().position;
 
-        pos = targetPos;
-        pos -= dist;
-        /*pos.x -= distX;
-        pos.y -= distY;
-        pos.z -= distZ;*/
 
         transform.position = pos;
-
-        transform.RotateAround(targetPos, new Vector3(0f, Input.GetAxis("Mouse X"), 0f), 50*Time.deltaTime);
-        transform.Rotate(new Vector3(-((Input.GetAxis("Mouse Y")*50)*Time.deltaTime), 0f, 0f));
     }
 }
