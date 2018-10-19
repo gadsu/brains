2ecd88b11370b2dc8271ba5d3b12fea280718606
Paul Manley <pjmanley671@gmail.com>
2018-10-19 18:04:44 -0500

    Modified the StealthHandler script to better the equation and to simplify updating the state and removed redundant checks.

--- Brains/Assets/Scripts/Player/StealthHandler.cs
+++ Brains/Assets/Scripts/Player/StealthHandler.cs
@@ -6,27 +6,28 @@ public class StealthHandler : MonoBehaviour
 {
     private float stealth_val;
     public float Stealth_val { get { return stealth_val; } } // lets other scripts read the stealth_val without accidently manipulating it's value.
-    private float offset, hidden;
+    private int offset, hidden;
+    public bool Hidden{ get { return Convert.ToBoolean(hidden); } }
 
     private void Awake()
     {
         /* Initializes 'simple' data variables. */
-        offset = 0f;
-        hidden = 0f;
+        offset = 0;
+        hidden = 0;
         stealth_val = 0f;
         /****************************************/
     }
 
-    private void UpdateHiddenState(bool state)
+    public void UpdateHiddenState(bool state)
     {
-        hidden = (state) ? 1f : 0f; // allows for places such as a pile of dead bodies to set the hidden state to true.
+        hidden = Convert.ToInt32(state); // allows for places such as a pile of dead bodies to set the hidden state to true.
     }
 
     public void UpdateStealthState(int playDead, int arms, int legs, int mvState)
     {
-        offset = (hidden > 0f && mvState != 5) ? 1f : 0f; // offsets the value of the states if not crawling.
+        offset = (hidden > 0f && mvState != 5) ? 1 : 0; // offsets the value of the states if not crawling.
 
         stealth_val = 
-            (hidden - playDead * (arms + legs)) + (((hidden - 1) * Mathf.Abs(5 - mvState) + 5 * playDead) / 2) - offset; // calculates the new stealth rate value.
+            (float)((hidden - playDead * (arms + legs)) + (((hidden - 1) * Mathf.Abs(5 - mvState) + 5 * (playDead + hidden) / 2) - offset)); // calculates the new stealth rate value.
     }
 }
