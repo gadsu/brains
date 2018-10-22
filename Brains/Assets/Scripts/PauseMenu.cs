b9944d03ed79c9f2d2428770e8f23d6539665398
Tristen Brinkman <42529082+Zuokan@users.noreply.github.com>
2018-10-22 18:43:36 -0500

    Setting Up Pause Menu
    Settiing up a pause menu to pause the game and all objects in game and to goto credits and quit the game.

---
+++ Brains/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour {
+
+    public static bool GamePaused = false;
+
+    public GameObject pauseMenuUI;
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+	}
+
+    void Resume()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GamePaused = false;
+    }
+
+    void Pause()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GamePaused = true;
+    }
+    
+}
