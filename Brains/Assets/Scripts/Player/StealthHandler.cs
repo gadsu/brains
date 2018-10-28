5cd73bdd2f6ea8a45e711372b675cc4aabc1c0a3
Detrian Hallem <detrian.hallem@trojans.dsu.edu>
2018-10-28 16:12:57 -0500

    Revert "Merge branch 'Models' of https://github.com/gadsu/brains into Models"
    This reverts commit 2c505b83ab77d5dc365b9309dfa71e582cbf53e3, reversing
    changes made to 1014129694ef15fb9dd2725181bb7b1096fd80d7.

--- Brains/Assets/Scripts/Player/StealthHandler.cs
+++ Brains/Assets/Scripts/Player/StealthHandler.cs
@@ -6,28 +6,27 @@ public class StealthHandler : MonoBehaviour
 {
     private float stealth_val;
     public float Stealth_val { get { return stealth_val; } } // lets other scripts read the stealth_val without accidently manipulating it's value.
-    private int offset, hidden;
-    public bool Hidden{ get { return Convert.ToBoolean(hidden); } }
+    private float offset, hidden;
 
     private void Awake()
     {
         /* Initializes 'simple' data variables. */
-        offset = 0;
-        hidden = 0;
+        offset = 0f;
+        hidden = 0f;
         stealth_val = 0f;
         /****************************************/
     }
 
-    public void UpdateHiddenState(bool state)
+    private void UpdateHiddenState(bool state)
     {
-        hidden = Convert.ToInt32(state); // allows for places such as a pile of dead bodies to set the hidden state to true.
+        hidden = (state) ? 1f : 0f; // allows for places such as a pile of dead bodies to set the hidden state to true.
     }
 
     public void UpdateStealthState(int playDead, int arms, int legs, int mvState)
     {
-        offset = (hidden > 0f && mvState != 5) ? 1 : 0; // offsets the value of the states if not crawling.
+        offset = (hidden > 0f && mvState != 5) ? 1f : 0f; // offsets the value of the states if not crawling.
 
         stealth_val = 
-            (float)((hidden - playDead * (arms + legs)) + (((hidden - 1) * Mathf.Abs(5 - mvState) + 5 * (playDead + hidden) / 2) - offset)); // calculates the new stealth rate value.
+            (hidden - playDead * (arms + legs)) + (((hidden - 1) * Mathf.Abs(5 - mvState) + 5 * playDead) / 2) - offset; // calculates the new stealth rate value.
     }
 }
