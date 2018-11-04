c7ca224d5c451e3879aaa1c67ba246c62a171e4d
Paul Manley <pjmanley671@gmail.com>
2018-11-04 17:27:07 -0600

    Added scripts for enemies to 'see' player and made it so you can see an enemies points of path.

---
+++ Brains/Assets/Scripts/Enemy/DetectPlayer.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Camera))]
+public class DetectPlayer : MonoBehaviour
+{
+    Camera m_camera;
+    Transform m_target;
+    Vector3 targetPosisition;
+
+    Ray m_ray;
+    RaycastHit m_out;
+
+    bool inView;
+    private void Awake()
+    {
+        m_camera = GetComponent<Camera>();
+        m_target = GameObject.Find("Player").GetComponent<Transform>();
+    }
+
+    private void Start()
+    {
+        m_camera.enabled = false;
+        inView = false;
+        m_ray = new Ray();
+        targetPosisition = new Vector3();
+    }
+
+    private void Update()
+    {
+        m_ray.origin = transform.position;
+        targetPosisition = m_target.position;
+        targetPosisition.x -= m_ray.origin.x;
+        targetPosisition.y -= m_ray.origin.y;
+        targetPosisition.z -= m_ray.origin.z;
+
+        Vector3 worldView = m_camera.WorldToViewportPoint(targetPosisition);
+        m_ray.direction = Vector3.RotateTowards(m_ray.origin, targetPosisition, Mathf.Infinity, Mathf.Infinity);
+
+        inView = (worldView.z <= 0f) ? 
+            false : (worldView.x >= 0f && worldView.x <= 1f) ?
+            true : false;
+        //Debug.Log(Vector3.Angle(m_ray.origin, m_target.position));
+    }
+
+    private void FixedUpdate()
+    {
+        if (inView)
+        {
+            //Debug.Log("Seeing player!");
+            Debug.DrawRay(m_ray.origin, m_ray.direction * Vector3.Distance(m_ray.origin, targetPosisition), Color.red);
+            if (Physics.Raycast(m_ray, out m_out, Vector3.Distance(m_ray.origin, targetPosisition)))
+            {
+                if (m_out.transform.CompareTag("Player"))
+                {
+                    Debug.Log("Hit the player");
+                }
+            }
+        }
+    }
+}
---
+++ Brains/Assets/Scripts/Enemy/PathTo.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class PathTo : MonoBehaviour
+{
+    NavMeshAgent m_agent;
+    public Vector3[] m_destinations;
+    Vector3 m_target;
+    int destinationI;
+
+    private void OnDrawGizmosSelected()
+    {
+        if (m_destinations.Length > 0)
+        {
+            foreach (Vector3 des in m_destinations)
+            {
+                Gizmos.color = Color.green;
+                Vector3 l_des = new Vector3()
+                {
+                    x = des.x,
+                    y = des.y,
+                    z = des.z
+                };
+                Gizmos.DrawSphere(l_des, .25f);
+            }
+        }
+    }
+    // Use this for initialization
+    private void Awake()
+    {
+        m_agent = GetComponent<NavMeshAgent>();
+    }
+    void Start ()
+    {
+        destinationI = 0;
+        m_target = (m_destinations.Length > 0) ? m_destinations[destinationI] : transform.position;
+        m_agent.SetDestination(m_target);
+	}
+
+    private void UpdateDestination()
+    {
+        if (m_destinations.Length > 0)
+        {
+            if (Mathf.Abs(m_agent.remainingDistance) < .1f)
+            {
+                //Debug.Log("Index position: " + destinationI + " remaining distance: " + m_agent.remainingDistance);
+                destinationI = (destinationI < m_destinations.Length - 1) ? destinationI + 1 : 0;
+                //Debug.DrawLine(transform.position, (transform.position + transform.forward), Color.green);
+                m_target = m_destinations[destinationI];
+                m_agent.SetDestination((m_target));
+                //Debug.Log(m_agent.remainingDistance);
+            }
+        }
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        UpdateDestination();
+	}
+}
