040eaa6d7bd416348eb44364f168d0546a6cdd1f
Paul Manley <pjmanley671@gmail.com>
2018-11-16 16:56:54 -0600

    Third and first person camera switching.

---
+++ Brains/Assets/Scripts/CameraHandler.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraHandler : MonoBehaviour
+{
+    Camera[] _cameras;
+    private void Awake()
+    {
+        _cameras = new Camera[2];
+        _cameras[0] = GameObject.Find("FirstPersonCamera").GetComponent<Camera>();
+        _cameras[1] = GameObject.Find("ThirdPersonCamera").GetComponent<Camera>();
+
+        _cameras[0].enabled = false;
+        _cameras[1].enabled = true;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            if (_cameras[0].enabled == true)
+            {
+                _cameras[1].enabled = true;
+                _cameras[0].enabled = false;
+            }
+            else if (_cameras[1].enabled == true)
+            {
+                _cameras[0].enabled = true;
+                _cameras[1].enabled = false;
+            }
+            else { }
+        }
+    }
+}
