6482e4c472f3a0676dc6b2767f51866d6927763c
pjmanley671 <pjmanley671@gmail.com>
2018-11-19 18:49:26 -0600

    Adding behaviors to destinations part 1.

---
+++ Brains/Assets/Scripts/Enemy/ArrayDestinations.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArrayDestinations : MonoBehaviour
+{
+    public Destination[] _destinations;
+}
---
+++ Brains/Assets/Scripts/Enemy/Destination.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Destination : MonoBehaviour
+{
+    public enum DestinationType : int
+    {
+        Pass = 0,
+        Stop = 1,
+        Turn = 2,
+        Rest = 3
+    }
+
+    public DestinationType _destinationType;
+    public Vector3 _destinationLocation;
+    private Color[] _destinationColors = {Color.green, Color.red, Color.cyan, Color.blue};
+
+    public float _size;
+
+    private void OnDrawGizmosSelected()
+    {
+        Color _color = _destinationColors[(int)_destinationType];
+        Gizmos.color = _color;
+        Gizmos.DrawSphere(_destinationLocation, _size);
+    }
+}
