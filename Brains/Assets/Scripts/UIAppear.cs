8ef95f3e0bc98d43df3567c189f97d106223acfd
Julian Buettgenback <jjulian1117@gmail.com>
2019-02-01 19:14:00 -0600

    made a script called UIappear where when player collides with the object, an image will appear in front of them and disappear when they are outside of it.

---
+++ Brains/Assets/Scripts/UIAppear.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIAppear : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject customImage;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.CompareTag("Player"))
+        {
+            customImage.SetActive(true);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            customImage.SetActive(false);
+        }
+    }
+
+}
