53bb6445e3c5b884a3fdb6c2c10c8f3adc28ab36
pjmanley671 <pjmanley671@gmail.com>
2019-02-13 18:53:56 -0600

    Modifying AI Pathing scripts to allow for idle behavior.

--- Brains/Assets/Scripts/Characters/Enemy/Destination.cs
+++ Brains/Assets/Scripts/Characters/Enemy/Destination.cs
@@ -16,4 +16,6 @@ public class Destination : System.Object
     public Vector3 _destinationLocation;
     private Color[] _typeColor = {Color.yellow, Color.green, Color.blue};
     public Color _destinationColor { get { return _typeColor[(int)_destinationType]; } }
+
+    public float idleTime;
 }
