2bdeafc333ed089149c992ce825f6811b645a0a7
Paul Manley <pjmanley671@gmail.com>
2019-03-21 10:06:26 -0500

    Adding bottle collision scripts.

---
+++ Brains/Assets/Scripts/BottleCollision.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BottleCollision : MonoBehaviour
+{
+    // Holds the reference for the object sounds.
+    public ObjectSounds sounds;
+
+    private void Awake()
+    { 
+        // initializes the object sounds.
+        sounds.InitSounds(gameObject);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // if the player is the collision.
+        if (collision.collider.CompareTag("Player") && collision.gameObject.name == "Spud")
+        {
+            // if there is a rigidbody associate with this gameObject.
+            if (GetComponent<Rigidbody>() != null)
+            {
+                GetComponent<Rigidbody>().
+                    AddForce(GetComponent<Rigidbody>().velocity, ForceMode.Impulse);
+
+                if (sounds.objectSounds.Capacity > 0)
+                    sounds.Play(sounds.objectSounds[Random.Range(0, sounds.objectSounds.Capacity - 1)]);
+
+                for (int i = 0; i < GetComponent<EnemiesInRange>().enemies.Capacity; i++)
+                {
+                    GetComponent<EnemiesInRange>().
+                        enemies[i].GetComponent<DetectPlayer>().
+                        UpdatingDetectionAmountFromSound((1f / Vector3.
+                        Distance(GetComponent<EnemiesInRange>().enemies[i].transform.position,
+                        gameObject.transform.position)));
+                }
+            }
+        }
+    }
+}
---
+++ Brains/Assets/Scripts/EnemiesInRange.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemiesInRange : MonoBehaviour
+{
+    [HideInInspector]
+    public List<GameObject> enemies;
+
+    private void Awake()
+    {
+        enemies = new List<GameObject>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        switch (other.gameObject.name.Substring(0, 3))
+        {
+            case "Tom ":
+            case "Mac ":
+            case "Sean": enemies.Add(other.gameObject);
+                enemies.TrimExcess();
+                break;
+            default: break;
+        }
+
+        Debug.Log("<color=cyan></color>" + other.gameObject.name);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        switch (other.gameObject.name.Substring(0, 3))
+        {
+            case "Tom ":
+            case "Mac ":
+            case "Sean":
+                enemies.Remove(other.gameObject);
+                enemies.TrimExcess();
+                break;
+            default: break;
+        }
+    }
+}
