c88fe606c42481044b48a62b32167a7928fb300b
Julian Buettgenback <jjulian1117@gmail.com>
2019-03-24 21:44:20 -0500

    Reimplemented the Tutorial script for the tutorial button in the scene menu

---
+++ Brains/Assets/tutorialSelection.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class tutorialSelection : MonoBehaviour
+{
+
+    private void Awake()
+    {
+        Cursor.visible = true;
+    }
+
+    public void PlayGame()
+    {
+        Debug.Log("Test Tutorial button.");
+        SceneManager.LoadScene("Tutorial");
+    }
+
+}
+
