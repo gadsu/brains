39023f4826fb9e15c59e75923aae94b82988d358
jpbuettgenback <38230465+jpbuettgenback@users.noreply.github.com>
2019-03-26 10:23:11 -0500

    Made load images and scripts for all levels in menu

---
+++ Brains/Assets/Scripts/Menu/LoadOnClick.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadOnClick : MonoBehaviour {
+    public GameObject loadingImage;
+    
+	public void LoadScene(string level)
+    {
+        loadingImage.SetActive(true);
+        Application.LoadLevel(level);
+    }
+}
+
---
+++ Brains/Assets/Scripts/Menu/LoadOnClickLevel2.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadOnClickLevel2 : MonoBehaviour
+{
+    public GameObject loadingImage;
+
+    public void LoadScene(string level)
+    {
+        loadingImage.SetActive(true);
+        Application.LoadLevel(level);
+    }
+}
