4af538ba7cfa954833edde949466a8b1405ead43
Paul Manley <pjmanley671@gmail.com>
2019-04-01 14:04:03 -0500

    Updating Build and remaking navmesh

--- Brains/Assets/Scripts/Enemy/AttackHitting.cs
+++ Brains/Assets/Scripts/Enemy/AttackHitting.cs
@@ -5,21 +5,37 @@ using UnityEngine;
 public class AttackHitting : MonoBehaviour
 {
     public EnemyBase tah;
+    public ObjectSounds train;
     public bool isTrain;
+
+    private void Awake()
+    {
+        if (train != null)
+        {
+            train.InitSounds(gameObject);
+        }
+    }
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player") && (tah.registerAttack || isTrain) && other.transform.name == "Spud")
+        if (other.CompareTag("Player") && other.transform.name == "Spud")
         {
             if(isTrain)
             {
+                train.Play("Hit");
                 // Camera tracks the *TRAIN* who killed spud.
                 GameObject.Find("GameStateController").GetComponent<GameStateHandler>().killer = gameObject;
+
+                train.Play("Victory");
             }
-            tah.SoundEvent("Hit");
-            if(Random.value>0.5f)
+
+            if (tah != null)
             {
-                tah.SoundEvent("Victory");
+                tah.SoundEvent("Hit");
+                if (Random.value > 0.5f)
+                {
+                    tah.SoundEvent("Victory");
+                }
             }
             GameObject.Find("GameStateController").GetComponent<GameStateHandler>().SetState(GameStateHandler.GameState.Lost);
         }
--- Brains/Assets/Scripts/Menu/CreditsMenu.cs
+++ Brains/Assets/Scripts/Menu/CreditsMenu.cs
@@ -3,6 +3,10 @@ using UnityEngine.SceneManagement;
 
 public class CreditsMenu : MonoBehaviour {
 
+    private void Awake()
+    {
+        Cursor.visible = true;
+    }
     public void ReturnMenu()
     {
         Debug.Log("Returning to Menu...");
