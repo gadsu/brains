57c4df2bb835f4c87e1899b3a24df256f59de288
Paul Manley <pjmanley671@gmail.com>
2019-04-01 11:42:52 -0500

    Train pathing script.

---
+++ Brains/Assets/Scripts/RailRoadNavigation.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+public class RailRoadNavigation : MonoBehaviour
+{
+    public Path directPath;
+    private int indexAlongPath;
+    private NavMeshAgent _agent;
+    private void Awake()
+    {
+        indexAlongPath = 0;
+        _agent = GetComponent<NavMeshAgent>();
+        if (directPath.pathPoints.Capacity > 0)
+        {
+            _agent.SetDestination(directPath.pathPoints[indexAlongPath].location);
+            _agent.destination = directPath.pathPoints[indexAlongPath].location;
+        }
+        else
+            Debug.Log("Missing Path on: " + name);
+    }
+
+    private void Update()
+    {
+        if (_agent.destination != directPath.pathPoints[indexAlongPath].location && _agent.remainingDistance < _agent.stoppingDistance + .1f)
+        {
+            _agent.SetDestination(directPath.pathPoints[indexAlongPath].location);
+            _agent.destination = directPath.pathPoints[indexAlongPath].location;
+
+            indexAlongPath++;
+            if (indexAlongPath >= directPath.pathPoints.Capacity) indexAlongPath = 0;
+        }
+    }
+}
